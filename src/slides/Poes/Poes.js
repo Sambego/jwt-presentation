5076f8870a0cb6e09146177267cd1ac50e6f8e38
Sambego <sambellen@gmail.com>
2019-03-06 11:48:29 +0100

    Add tweet me to poes slide

--- src/slides/Poes/Poes.js
+++ src/slides/Poes/Poes.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Slide from "../../components/Slide";
+import Subtitle from "../../components/Subtitle";
 
 import poes from '../../images/poes.jpg';
 
@@ -10,6 +11,7 @@ const Poeslide = ({ text, background, color, note, style }) => (
     <div style={{position: 'relative'}}>
       <img src={poes} alt="Cat stickers" style={{width: '100vw', height: '100vh', objectFit: 'cover'}} />
       <div style={{position: 'absolute', top: 0, width: '100vw', height: '100vh', background: '#5FB3B3', opacity: .5}} />
+      <Subtitle style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate3d(-50%, -50%, 0)', color: '#fff'}}>Tweet me @sambego</Subtitle>
     </div>
   </Slide>
 );
